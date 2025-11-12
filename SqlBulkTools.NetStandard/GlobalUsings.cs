67b6325e8fb43bbba9762df3efc1befc22d7f43c
fretje <github@fretje.be>
2025-11-12 18:06:12 +0100

    Remove ReleaseSystemData

--- SqlBulkTools.NetStandard/GlobalUsings.cs
+++ SqlBulkTools.NetStandard/GlobalUsings.cs
@@ -1,10 +1,6 @@
 ﻿global using System.Data;
 global using System.Linq.Expressions;
-#if RELEASESYSTEMDATA
-global using System.Data.SqlClient;
-#else
 global using Microsoft.Data.SqlClient;
-#endif
 global using SqlBulkTools.Enumeration;
 
 using System.Runtime.CompilerServices;
